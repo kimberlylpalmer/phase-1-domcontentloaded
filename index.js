785bfb4c85ca4ac640eee87a1fb16c242bb22eec
Kimberly L Palmer <kimberlylpalmer@gmail.com>
2023-09-17 13:41:18 -0700

    phase-2-domcontentloaded complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('The DOM is in the house')
+    document.querySelector('p').textContent = "This is really cool!"
+
+})
+console.log('haha i\'m first')
