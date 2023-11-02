906f1fcedb41abf3ca859c3f552aa40dc7aa82bc
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-11-02 13:25:12 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 // Your code goes here
-document.addEventListener("DOMContentLoaded", () => {
-    console.log('The DOM is in the house')
-    document.querySelector('p').textContent = "This is really cool!"
 
-})
-console.log('haha i\'m first')
+document.addEventListener("DOMContentLoaded", changeText);
+
+let textBox = document.getElementById("text")
+
+function changeText(e) {
+    textBox.textContent = "This is really cool!"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     console.log('The DOM is in the house')
+//     document.querySelector('p').textContent = "This is really cool!"
+
+// })
+// console.log('haha i\'m first')
